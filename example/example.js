7c30c4d8b8c465f4df7e46697db4fea434f739a4
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-17 22:50:30 +0400

    [typo] example

--- example/example.js
+++ example/example.js
@@ -1,5 +1,5 @@
 var mwc_core = require('./../index.js');
-var MWC = new mwc_core(require('./config.json')[(process.env.NODE_ENV) ? ('process.env.NODE_ENV') : 'development']);
+var MWC = new mwc_core(require('./config.json')[(process.env.NODE_ENV) ? (process.env.NODE_ENV) : 'development']);
 
 
 MWC.listen(MWC.get('port'), function () {
