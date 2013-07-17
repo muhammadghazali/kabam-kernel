d2a58d1b94acd2358baba6449054fcd751beb596
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-18 01:10:37 +0400

    [features] - mongoose user handing - testing

--- index.js
+++ index.js
@@ -1,19 +1,101 @@
+var EventEmitter = require("events").EventEmitter,
+    util = require("util");
+
 function MWC(config) {
+    EventEmitter.call(this);
+    var thisMWC=this;
     var express = require('express'),
         path = require('path'),
-        packages = require(path.dirname(module.parent.filename) + '/package.json').dependencies;
+        packages = require(path.dirname(module.parent.filename) + '/package.json').dependencies,
         url = require('url'),
         passport = require('passport'),
         LocalStrategy = require('passport-local').Strategy,
         HashStrategy = require('passport-hash').Strategy,
         GoogleStrategy = require('passport-google').Strategy,
+        UsersModel = require('./models/USERS.js'),
+        DocumentsModel = require('./models/DOCUMENTS.js'),
         RedisStore = require('connect-redis')(express),
         flashMiddleware = require('connect-flash'),
+        mongoose = require('mongoose'),
         app = express(),
         pluginsInUse=[];
 
-
+//init models, DI of models into request variable
+    mongoose.connect(config.mongo_url);
+    var db=mongoose.connection;
+    db.on('connect', function (err) {
+        if (err) throw err;
+        console.log('Mongo connection established!');
+        thisMWC.emit('ready');
+    });
+    db.on('error', function (err) {
+        console.error('Mongo connection error!');
+        console.error(err);
+    });
+    var Users=UsersModel(db, config);
+    var Documents=DocumentsModel(db, config);
+    app.use(function(request,response,next){
+        request.MODEL={
+            'Users':Users,
+            'Documents':Documents
+        };
+        next();
+    });
 //setting passport middleware
+
+    passport.use(new LocalStrategy(
+        function (username, password, done) {
+            Users.findOne({username:username, active:true}, function (err, user) {
+                if(err){
+                    return done(err,false,{'message':'Database broken...'});
+                } else {
+                    if(user){
+                        if(user.verifyPassword(password)){
+                            return done(null,user,{message:'Welcome, '+user.username});
+                        } else {
+                            return done(null, false, { message:'Access denied!' });
+                        }
+                    } else {
+                        return done(null, false, { message:'Access denied!' });
+                    }
+                }
+            });
+        }
+    ));
+
+
+    passport.use(new GoogleStrategy({
+            returnURL: config.hostUrl+'auth/google/return',
+            realm: config.hostUrl
+        },
+        function(identifier, profile, done) {
+            var email=profile.emails[0].value;
+            console.log(profile);
+            Users.findOne({'email':email, active:true},function(err,userFound){
+                console.log(userFound);
+                if(userFound){
+                    done(err, userFound,{message:'Welcome, '+userFound.username});
+                } else {
+                    //model.UserModel.create({email:email},function(err,userCreated){
+                        done(err,false,{ message:'Access denied!' });//todo - i am not sure if user can register by singing in with Google Acount
+                    //});
+                }
+            });
+        }
+    ));
+
+
+//Storing user in session
+    passport.serializeUser(function (user, done) {
+        done(null, user.username);
+    });
+
+    passport.deserializeUser(function (username, done) {
+        Users.findOne({username:username}, function (err, user) {
+            done(err, user);
+        });
+    });
+//-------
 
 //passport.use()
 //
@@ -56,8 +138,8 @@ function MWC(config) {
 
 //dependency injection of plugins
     for (var x in packages) {
-        var pluginParameters = /^mwc_plugin_([a-z0-9_]+)/.exec(x);
-        //this regex is parsed like it for our pluggins [ 'mwc_plugin_example','example',index: 0,input: 'mwc_plugin_example' ]
+        var pluginParameters = /^mwc_plugin_([a-z0-9_]+)$/.exec(x);
+        //this regex is parsed like it for our plugins [ 'mwc_plugin_example','example',index: 0,input: 'mwc_plugin_example' ]
         if (pluginParameters) {
             require(pluginParameters[0])(app, '/' + pluginParameters[1],config);
             pluginsInUse.push({
@@ -86,12 +168,26 @@ function MWC(config) {
             res.send('Error 503. There are problems on our server. We will fix them soon!');//todo - change to our page...
         });
     });
-
+    app.get('/my',function(request,response){
+        if(request.user){
+            response.json(request.user);
+        } else {
+            response.send(403);
+        }
+    });
+    app.get('/auth/google', passport.authenticate('google'));
+    app.get('/auth/google/return',passport.authenticate('google', { failureRedirect: '/', successRedirect: '/' }));
+    app.post('/logoff',function(request,response){
+        request.logout();
+        response.send(200,'OK');
+    });
 
     app.get('*', function (request, response) {
         response.send(404);
     });
     return app;
 }
+
+util.inherits(MWC, EventEmitter);
 
 module.exports = exports = MWC;
---
+++ models/USERS.js
@@ -0,0 +1,101 @@
+var mongoose = require('mongoose'),
+    async = require('async'),
+    rand = require("generate-key"),
+    crypto = require('crypto'),
+    Schema = mongoose.Schema,
+    moment = require('moment');
+
+
+function md5(str) {
+    return crypto.createHash('md5').update(str).digest('hex').toString();
+}
+
+module.exports = exports = function (db, config) {
+    var UserSchema = new Schema({
+        active: Boolean,
+        activeDate: Date,
+        accounts: [
+            {
+                provider: String,
+                id: String,
+                displayName: String,
+                name: {
+                    familyName: String,
+                    givenName: String,
+                    middleName: String
+                },
+                emails: [
+                    {
+                        value: String,
+                        type: {
+                            type: String
+                        }
+                    }
+                ],
+                phoneNumbers: [
+                    {
+                        value: String,
+                        type: {
+                            type: String
+                        }
+                    }
+                ],
+                photos: [
+                    {
+                        value: String
+                    }
+                ]
+            }
+        ],
+        confirmation: {
+            string: String,
+            date: Date
+        },
+        username: {string: String},
+        email: {string: String},
+        salt: {string: String},
+        password: {string: String},
+        groups: [Number]
+    });
+
+    UserSchema.index({
+        email: 1,
+        username: 1
+    });
+
+    UserSchema.methods.generateConfirmationLink = function (callback) {
+        this.confirmation.string = rand.generateKey();
+        this.confirmation.date = new Date();
+        this.save(callback);
+        return;
+    };
+
+    UserSchema.methods.activateUser = function (confirmationString, callback) {
+        var err;
+        if (typeof confirmationString !== 'undefined') {
+            if (confirmationString !== this.confirmation.string) {
+                err = new Error('Confirmation Strings don\'t match');
+                callback(err);
+            }
+            var originalDate = moment(this.confirmation.date),
+                now = moment(),
+                expired = (typeof config.confirmationLinkExpire === 'undefined') ? 1 :
+                    config.confirmationLinkExpire;
+
+            if (now.isAfter(originalDate.add('hours', expired))) {
+                err = new Error('The link has expired.');
+                callback(err);
+            }
+        }
+        this.confirmation = null;
+        this.activeDate = new Date();
+        this.active = true;
+        this.save(callback);
+        return;
+    };
+
+    UserSchema.methods.verifyPassword = function(password){
+        return md5(this.salt+password)==this.password;
+    }
+    return mongoose.model('User', UserSchema);
+}
