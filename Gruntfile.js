5e62e1c6bbb8d99e96505653f1ed9163d686441f
T. Budiman <tbudiman@gmail.com>
2013-07-20 20:47:02 +0700

    chore(gruntfile): add jshint and vows

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'index.js', 'example/**/*.js', 'models/**/*.js', 'public/**/*.js', 'routes/**/*.js', 'test/**/*.js']
+    },
+    vows: {
+      all: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-vows');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
