fe2b72d87e26b32e38d49e4b12b964635dbfd2be
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-21 12:55:17 +0400

    extending example

--- example/example.js
+++ example/example.js
@@ -29,9 +29,50 @@ MWC.setAppMiddlewares(['development','staging'],function(core){
 //we add some routes
 MWC.extendAppRoutes(
         function (core) {
-        core.app.get('/', function (req, res) {
-            res.send('Hello! TempVar is '+core.app.get('TempVar'));
-        })}
+            core.app.get('/', function (req, res) {
+                var authByGoogleString=(req.user)?
+                    '<li><a href="/my">See your profile</a></li>'
+                    :'<li><a href="/auth/google">Auth by google</a></li>';
+
+                res.send('<html>' +
+                    '<head>MyWebClass Core Example</head>' +
+                    '<body>' +
+                    '<p>TempVar is ' + core.app.get('TempVar') + '</p>' +
+                    '<p>Hello, friend! You can do this things:</p><ul>' +
+                    '<li>See current <a href="/time">time</a>.</li>' +
+                    '<li>See <a href="/team">team</a> on this server.</li>' +
+                    '<li>See all <a href="/redis">redis</a> keys stored.</li>' +
+                    authByGoogleString+
+                    '<li>See this server <a href="/config">parameters</a>.</li>' +
+                    '<li>See users <a href="/api/users">api endpoint</a> on this server.</li>' +
+                    '<li>See <a href="/plugins">plugins</a> installed on this server.</li>' +
+                    '<li><a href="/honeypot">Notify</a> admin of your presense.</li>' +
+                    '<li>See this application <a href="https://github.com/mywebclass/mwc_plugin_example">source on Github</a></li>' +
+                    '</ul></body>' +
+                    '</html>');
+            });
+
+            //we use Mongoose Model in this route
+            core.app.get('/team', function (request, response) {
+                request.MODEL.Users.find({active: 1}, function (err, users) {
+                    if (err) throw err;
+                    response.json({'Team': users});
+                });
+            });
+            //we use exposed Redis client. In a rather stupid way.
+            core.app.get('/redis', function (request, response) {
+                request.redisClient.keys('*', function (err, keys) {
+                    response.json(keys);
+                });
+            });
+
+            //making mousetrap - when user visits this url, MWC emmits the event
+            core.app.get('/honeypot', function (request, response) {
+                request.emitMWC('honeypot accessed', 'Somebody with IP of ' + request.ip + ' accessed the honeypot');
+                response.send('Administrator was notified about your actions!');
+            });
+
+        }
 );
 
 //api/user works to!!!
