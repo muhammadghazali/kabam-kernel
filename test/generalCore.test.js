c60201f3f21700cdd975705298dbb65ccbd05d6e
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-07-24 16:16:56 +0400

    [feature] - unit tests for extending appliction core and application parameters

--- test/generalCore.test.js
+++ test/generalCore.test.js
@@ -7,6 +7,29 @@ var should = require('should'),
   blanket = require('blanket');
 
 var MWC = new mwcCore(config);
+var extendCoreFunction = function(core){
+  core.sum=function(a,b){
+    return (a+b);
+  };
+};
+MWC.extendCore(extendCoreFunction);
+
+var extendAppParametersFunction1 = function (core) {
+  core.app.set('TempVar1', 'TempVar1');
+};
+
+var extendAppParametersFunction2 = function (core) {
+  core.app.set('TempVar2', 'TempVar2');
+};
+
+var extendAppParametersFunction3 = function (core) {
+  core.app.set('TempVar3', 'TempVar3');
+};
+
+MWC.setAppParameters(['development', 'staging'], extendAppParametersFunction1);
+MWC.setAppParameters('development', extendAppParametersFunction2);
+MWC.setAppParameters('production', extendAppParametersFunction3);
+
 MWC.listen(3000);
 
 describe('mwcCore', function() {
@@ -145,11 +168,48 @@ describe('mwcCore', function() {
 
   describe('#MWC.extendCore()', function() {
 
-    it('to be created', function() {
-      throw new Error('Not implemented');
+    it('adds the extending core function to array of MWC.setCoreFunctions', function() {
+      MWC.setCoreFunctions.should.be.an.instanceOf(Array);
+      MWC.setCoreFunctions.should.include(extendCoreFunction);
     });
 
+    it('actully adds new functions to #MWC',function(){
+      MWC.sum.should.be.a('function');
+      MWC.sum(2,2).should.equal(4);
+    });
   });
+
+
+  describe('#MWC.setAppParameters()',function(){
+    it('adds the desired functions to MWC.setAppParametersFunctions',function(){
+      if(typeof process.env.NODE_ENV != 'undefined'){
+        process.env.NODE_ENV.should.be.equal('development');
+      }
+      MWC.setAppParametersFunctions.should.be.an.instanceOf(Array); //test fails, but program works. who nows why?
+    });
+    //todo - fails, because mocha is NOT vows
+    it('it set extendAppParametersFunction1 to development environment',function(){
+      MWC.setAppParametersFunctions.should.include({'environment':'development', 'settingsFunction':extendAppParametersFunction1});
+    });
+    it('it set extendAppParametersFunction1 to staging environment',function(){
+      MWC.setAppParametersFunctions.should.include({'environment':'staging',     'settingsFunction':extendAppParametersFunction1});
+    });
+    it('it set extendAppParametersFunction2 to development environment',function(){
+      MWC.setAppParametersFunctions.should.include({'environment':'development', 'settingsFunction':extendAppParametersFunction2});
+    });
+    it('it set extendAppParametersFunction3 to production environment',function(){
+      MWC.setAppParametersFunctions.should.include({'environment':'production',  'settingsFunction':extendAppParametersFunction3});
+    });
+
+    it('actually works',function(){
+      MWC.app.get('TempVar1').should.equal('TempVar1');
+      MWC.app.get('TempVar2').should.equal('TempVar2');
+      if(typeof MWC.app.get('TempVar3') != 'undefined'){
+        throw new Error('We set app parameter for wrong environment!');
+      }
+    });
+  });
+
 
   describe('#MWC.setAppMiddlewares()', function() {
 
