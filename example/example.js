8a8ebd225d4a8b6990308449832b12ccf2091fd0
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-04 12:37:01 +0400

    [fix] - new syntax in example

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 var mwcCore = require('./../index.js');
 //setting up the config
-var MWC = new mwcCore(require('./config.json')[(process.env.NODE_ENV) ? (process.env.NODE_ENV) : 'development']);
+var MWC = mwcCore(require('./config.json')[(process.env.NODE_ENV) ? (process.env.NODE_ENV) : 'development']);
 
 //we extend the mwc_core instance
 MWC.extendCore(function (core) {
