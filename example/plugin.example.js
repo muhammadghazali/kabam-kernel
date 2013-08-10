839dab7032a88ad024dd526414de0be29ec24416
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-10 13:49:53 +0400

    [documentation] - for plugin

--- example/plugin.example.js
+++ example/plugin.example.js
@@ -153,7 +153,6 @@ exports.middleware = [
  * @type {function}
  * @description
  * Extend appliction routes, sends the value as argument to a function mwc.extendRoutes
- * @param {mwc} mwc
  * @example
  * ```javascript
  *  exports.routes = function(mwc){
