0f75c4e84cab25b64561acdf5f90dff3a9435841
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-13 14:43:01 +0400

    [feature] - script to create Procfile

---
+++ bin/createProcFile.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+/*
+This script creates procfile to run this application
+It requires the application to be able to be started by npm start
+https://npmjs.org/doc/cli/npm-start.html
+*/
+
+var cwd = process.cwd(),
+  fs = require('fs'),
+ packageJson = require(cwd+'/package.json');
+
+
+
+if(packageJson.scripts && packageJson.scripts.start){
+  var startCommand = packageJson.scripts.start;
+  console.log('We found commnad to start this application  - '+startCommand);
+
+  fs.exists(cwd+'/Procfile',function(isExists){
+    if(isExists){
+      console.error('We already have Procfile created!');
+      process.exit(1);
+    } else {
+     var procFileText='web: '+startCommand+'\n';
+      fs.writeFile(cwd+'/Procfile',procFileText,function(err){
+        if (err) throw err;
+        console.log('Procfile created successfully! You can edit it if you want.');
+        process.exit(0);
+      });
+    }
+  });
+
+} else {
+  console.error('We need this project to be run by `npm start`');
+  process.exit(1);
+}
---
+++ bin/runProd.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: nap
+ * Date: 8/13/13
+ * Time: 1:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
