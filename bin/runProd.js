9cf973c9e25424e0c3723202cf2579b81174065f
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-13 14:46:21 +0400

    [feature] runProd - testing

--- bin/runProd.js
+++ bin/runProd.js
@@ -1,7 +1,20 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: nap
- * Date: 8/13/13
- * Time: 1:57 PM
- * To change this template use File | Settings | File Templates.
- */
+#!/usr/bin/env node
+
+var os = require('os'), //cpus
+  child_process = require('child_process'),
+  cpus=os.cpus(),
+  numCPUs=cpus.length;
+
+
+console.log('You have '+numCPUs+' CPU cores available.');
+console.log('Scaling application for '+numCPUs+' process, 1 process per 1 core.');
+
+
+var nf = child_process.spawn('node',['node_modules/.bin/nf.js', ('web='+numCPUs)],function(err,stdout,stderr){
+  if(err) throw err;
+  console.log(stderr);
+  console.log(stdout);
+});
+
+//setTimeout(nf.disconnect, 1000);
+
