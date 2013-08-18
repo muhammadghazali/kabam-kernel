e4b0631d1c71dcb3a37a09917b37b0914c06b9d8
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-08-18 22:33:51 +0400

    [feature] - placeholder for CLI system

---
+++ bin/kabamDeploy.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+/**
+ * Script to deploy application, creates basic folder structure
+ *
+ * public/
+ *   javascript/
+ *   css/
+ *   images/
+ *
+ * views/
+ *   layout.html
+ *
+ * config.js
+ *
+ *
+ */
+ 
+console.log('Deploy application');
