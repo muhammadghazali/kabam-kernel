dbafd3759e70482fac09735652d892990a7bda56
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-08-19 22:32:36 +0400

    [fix] kabam - > kabamUser. Now it do only user related tasks, not deployment

--- bin/kabamUser.js
+++ bin/kabamUser.js
@@ -3,32 +3,13 @@ var program = require('commander'),
   pkg = require('../package.json'),
   cwd = process.cwd(),
   fs = require('fs'),
-  colors = require('colors'),
-  packageJson = require(cwd+'/package.json');
+  colors = require('colors');
 
 program
   .version(pkg.version)
   .usage('[command] <parameters ...>')
   .option('-v, --verbose',' be verbose')
 ;
-
-program
-  .command('create')
-  .description('Create basic application structure in current directory')
-//  .option("-s, --setup_mode [mode]", "Which setup mode to use")
-  .action(function(){
-    console.log('creating');
-    process.exit(0);
-  });
-
-program
-  .command('publishAssets')
-  .description('Publish modules templates, css, javascripts and images')
-  .action(function(){
-    console.log('Publishing...');
-    process.exit(0);
-
-  });
 
 program
   .command('signup <username> <email> <password> [isVerified]')
@@ -70,37 +51,6 @@ program
     process.exit(0);
   });
 
-program
-  .command('procfile')
-  .description('create Procfile for Foreman or nodeForeman')
-  .action(function(){
-    if(packageJson.scripts && packageJson.scripts.start){
-      var startCommand = packageJson.scripts.start;
-//      console.log('We found command to start this application  - '+startCommand);
-
-      var procFileText='web: '+startCommand+'\n';
-      fs.exists(cwd+'/Procfile',function(isExists){
-        if(isExists){
-          console.error('We already have Procfile created!'.red);
-          process.exit(1);
-        } else {
-//          console.log('Trying to write');
-//          console.log(procFileText);
-//          console.log(cwd+'/Procfile');
-
-          fs.writeFile(cwd+'/Procfile',procFileText,function(err){
-            if (err) throw err;
-            console.log('Procfile created successfully! You can edit it if you want.'.green);
-            process.exit(0);
-          });
-        }
-      });
-
-    } else {
-      console.error('We need this project to be run by `npm start`'.red);
-      process.exit(1);
-    }
-  });
 
 program.on('--help', function(){
   console.log(' Examples');
@@ -113,7 +63,7 @@ program.on('--help', function(){
 
 
 program.parse(process.argv);
-console.log(('Kabam-Cli version '+pkg.version).bold.yellow.redBG);
+console.log(('KabamUser version '+pkg.version).bold.yellow.redBG);
 if(process.argv.length===2){
   program.outputHelp();
 }
