e2b23a4f94a459bc2be61e603b898f5dbd94bae9
T. Budiman <tbudiman@gmail.com>
2013-11-10 02:36:22 +0700

    fix(model-loader): use type checking instead of exception

--- core/model-loader.js
+++ core/model-loader.js
@@ -1,15 +1,20 @@
 exports.name = 'kabam-core-model-loader';
-exports.core = function(kernel){
+exports.core = function(kernel) {
+  if (!kernel.mongoose) {
+    throw new Error('Mongoose not initialized');
+  }
+
   kernel.model = {};
 
   Object.keys(kernel.extensions.models).forEach(function(name) {
+
     var schema = kernel.extensions.models[name](kernel);
-    try {
-      // TODO: maybe this should be done in the mongoose plugin
-      // for now we just assume that mongoConnection is always present
+
+    if (schema instanceof kernel.mongoose.Schema) {
+      // instantiate the model if its a mongoose schema
       kernel.model[name] = kernel.mongoConnection.model(name, schema);
-    } catch(e) {
-      // What if we don't want mongoose models?
+    } else {
+      // assume that it is already a model constructor
       kernel.model[name] = schema;
     }
   });
